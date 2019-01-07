e7f9a27a9c33d13b1159764393effe616648f379
wanying.wang <wanying.wang@17zuoye.com>
2019-01-07 17:27:20 +0800

    :wrench: update

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{msg}}
+    {{isDone}}
+    {{sentence}}
+    <!-- {{myFavoriteNumber}} -->
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({})
+export default class App extends Vue {
+  data () {
+    return {
+      like: 999
+    }
+  }
+  msg:string = 'sdfds'
+  isDone:boolean = false
+  k:number =8
+  sentence:string = `hello ,my name is ${this.msg}`
+  // myFavoriteNumber:string | number
+  alertName ():void {
+    // alert('hello')
+  }
+  mounted () {
+    this.alertName()
+    // this.myFavoriteNumber = 6
+  }
+  // 联合类型
+
+  // myFavoriteNumber = 'six'
+  // myFavoriteNumber = 6
+  // 接口
+  // interface person{
+  //   name:string;
+  //   age:number;
+  // }
+  onClick ():void {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
